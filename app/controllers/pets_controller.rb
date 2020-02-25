f02d0f76a0b678da433a6f83bda35f880e8f601a
Dave Winniford <dwinniford@gmail.com>
2020-02-25 13:24:17 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,7 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    if params[:owner][:name].length > 0 
     binding.pry 
     redirect to "pets/#{@pet.id}"
   end
